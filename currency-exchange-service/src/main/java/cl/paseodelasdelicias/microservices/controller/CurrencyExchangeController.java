c15b440952b34e0c5813452f598613c30fadd3c9
dutreras <dutreras369@gmail.com>
2022-07-10 18:47:43 -0400

    add db test

--- currency-exchange-service/src/main/java/cl/paseodelasdelicias/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/cl/paseodelasdelicias/microservices/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package cl.paseodelasdelicias.microservices.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,16 @@ import cl.paseodelasdelicias.microservices.model.ExchangeValue;
 
 @RestController
 public class CurrencyExchangeController {
-		
+	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retirebeExchangeValue(@PathVariable String from, @PathVariable String to ) {
-		return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		int port = Integer.parseInt(environment.getProperty("local.server.port"));
+		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65), port);
+		
+		return exchangeValue;
 	}
 }
--- currency-exchange-service/src/main/java/cl/paseodelasdelicias/microservices/model/ExchangeValue.java
+++ currency-exchange-service/src/main/java/cl/paseodelasdelicias/microservices/model/ExchangeValue.java
@@ -2,6 +2,11 @@ package cl.paseodelasdelicias.microservices.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,10 +16,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "exchange_value")
 public class ExchangeValue {
 	
+	@Id
 	private Long id;
+	
+	@Column(name = "currency_from")
 	private String from;
+	
+	@Column(name = "currency_to")
 	private String to;
+	
+	@Column(name = "conversion_multiple")
 	private BigDecimal conversionMultiple;
+	
+	private int port;
 }
