07f4346fe0eca8b654e98fac5fe8a1ed0aecd424
dutreras <dutreras369@gmail.com>
2022-07-24 11:15:19 -0400

    refactor

--- currency-exchange-service/src/main/java/cl/paseodelasdelicias/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/cl/paseodelasdelicias/microservices/controller/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package cl.paseodelasdelicias.microservices.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +19,7 @@ public class CurrencyExchangeController {
 	private ExchangeValueRepository repository;
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public ExchangeValue retirebeExchangeValue(@PathVariable String from, @PathVariable String to ) {
+	public ExchangeValue retireveExchangeValue(@PathVariable String from, @PathVariable String to ) {
 		
 		int port = Integer.parseInt(environment.getProperty("local.server.port"));
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
